15b2f09fd7ae2da621408de8f518b72cbe070ade
gbox3d <gbox3d@gmail.com>
2019-06-11 15:12:35 +0900

    mask 예제추가

---
+++ basic/6.mask.mjs
@@ -0,0 +1,21 @@
+import cv from 'opencv4nodejs'
+
+// segmenting by skin color (has to be adjusted)
+const skinColorUpper = hue => new cv.Vec(hue, 0.8 * 255, 0.6 * 255);
+const skinColorLower = hue => new cv.Vec(hue, 0.1 * 255, 0.05 * 255);
+
+
+let handImg = cv.imread('../res/hand5.png')
+
+const imgHLS = handImg.cvtColor(cv.COLOR_BGR2HLS);
+cv.imshowWait('hls',imgHLS)
+
+//범위내 마스킹 처리 
+const rangeMask = imgHLS.inRange(skinColorLower(0), skinColorUpper(15));
+cv.imshowWait('rangeMask',rangeMask)
+
+const blurred = rangeMask.blur(new cv.Size(10, 10));
+cv.imshowWait('blurred',blurred)
+
+const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+cv.imshowWait('thresholded',thresholded)
