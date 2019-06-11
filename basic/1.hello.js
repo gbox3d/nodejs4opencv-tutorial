8be902e5f6ae136403818aee9de2957ac4228e52
gbox3d <gbox3d@gmail.com>
2019-06-11 15:12:23 +0900

    es6

--- basic/1.hello.js
+++ basic/1.hello.js
@@ -1,7 +1,11 @@
 
 const cv = require('opencv4nodejs')
+// import {cv} from ''
+// import {fs} from 'fs'
 
-const mat = cv.imread('../res/Lenna.png');
+// console.log(fs)
+
+const mat = cv.imread('../res/Lenna.png') //이미지 읽어 메트릭스로 만들기 
 
 //cv.imwrite('./img.png', mat);
-cv.imshowWait("out", mat);
+cv.imshowWait("out", mat) //키입력 들어올때까지 기다리기 
--- readme.md
+++ readme.md
@@ -1,13 +1,20 @@
-### nodejs 용 opencv 예제
+## nodejs 용 opencv 예제(opencv4nodejs)
 
-#### 설치
+### 설치
 
 ```bash
 npm install opencv4nodejs --save
 ```
-
-
 node4opencv 는 자동으로 opencv 를 설치해준다.  
+그러므로 별도의 다른 설치가 필요없다.
+
+### 기타 
+
+es6 문법을 지원하는 mjs 파일 실행시키는 방법  
+```bash
+node --experimental-modules 6.mjs
+```
+
 
 
 ### 참고
