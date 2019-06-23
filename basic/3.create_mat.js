68b1db1a7c46b07f3ce0710b7f75f493b6f5d4b8
gbox3d <gbox3d@gmail.com>
2019-06-23 14:03:42 +0900

    3.mat 예제 수정

--- basic/3.create_mat.js
+++ basic/3.create_mat.js
@@ -2,5 +2,17 @@ const cv = require('opencv4nodejs')
 const mat = new cv.Mat(256,256, cv.CV_8UC3,[255, 255, 255]); //BGR
 
 
+mat.putText(
+    String("opencv"),
+    new cv.Point(32, 128),
+    cv.FONT_HERSHEY_SIMPLEX,
+    1,
+    {
+        // fontScale : 1,
+        color: new cv.Vec(0, 0, 0),
+        thickness: 1
+    }
+);
+
 //cv.imwrite('../temp/out.png', mat);
 cv.imshowWait("out", mat);
