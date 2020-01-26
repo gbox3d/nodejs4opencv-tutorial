9cf27f62aaa75768201c9e6e17f22a2fea1ba7a9
gbox3d <gbox3d@gmail.com>
2020-01-26 12:03:00 +0900

    ...

--- basic/1.hello.js
+++ basic/1.hello.js
@@ -1,14 +1,14 @@
+const cv = require('opencv4nodejs');
 
-const cv = require('opencv4nodejs')
+console.log( `opencv version ${cv.version.major}.${cv.version.minor}.${cv.version.revision}` );
+
 const mat = cv.imread('../res/Lenna.png') //이미지 읽어 메트릭스로 만들기 
-
 let _mat_gray = mat.cvtColor(cv.COLOR_BGR2GRAY)
-
 
 cv.imwrite('../temp/out.png', _mat_gray);
 
 console.log('done : gray img out')
 
-//cv.imshowWait("out", mat) //키입력 들어올때까지 기다리기 
+cv.imshowWait("out", mat) //키입력 들어올때까지 기다리기 
 
 
