baf845c306cdbf6293642d6e5dc53a76a714f043
gbox3d <gbox3d@gmail.com>
2020-01-30 16:14:46 +0900

    소스 정리

--- adv/countours/1.moments.js
+++ adv/countours/1.moments.js
@@ -1,3 +1,12 @@
+/*
+
+countous 의 moment 를 이용해서 중점 , 크기, 경계박스 구하기 예제 
+
+참고 : https://blog.naver.com/samsjang/220517848698
+
+
+
+*/
 const cv = require('opencv4nodejs')
 
 let srcMat = cv.imread('../../res/contour1.png')
@@ -22,57 +31,106 @@ const red = new cv.Vec(0, 0, 255);
 const _outImg = imgGray.cvtColor(cv.COLOR_GRAY2BGR);
 
 
-let contours_pt_list = contours.reduce((acc,cur)=> {
-
-  // console.log(`isconvex : ${cur.isConvex}`)
-
-  if( cur.area > 200) {
+// drawContours 함수 인자로 쓸 배열 생성 
+let contours_pt_list = contours.reduce((acc, cur) => {
+  //면적이 200이상만 받기 
+  if (cur.area > 200) {
     acc.push(cur.getPoints())
   }
   return acc;
+}, []);
 
-},[]);
+//윤각선 그리기 
+_outImg.drawContours(contours_pt_list, -1, red, 3);
 
-_outImg.drawContours(   contours_pt_list ,-1,red,3);
 
-let _contour = contours[0];
-let _mnt = contours[0].moments();
+//각 윤각선에 대한 정보 구하기 
+contours.forEach((_contour, index) => {
 
-console.log(_contour);
-console.log(_mnt);
+  // let _contour = contours[0];
+  let _mnt = _contour.moments();
 
-//무게 중심구하기 
-let cx = _mnt.m10 /_mnt.m00;
-let cy = _mnt.m01 /_mnt.m00;
+  console.log(_contour);
+  console.log(_mnt);
 
-_outImg.drawCircle(new cv.Point2(cx, cy), 8,
-  {
-    color: green,
+  //무게 중심구하기 
+  let cx = _mnt.m10 / _mnt.m00;
+  let cy = _mnt.m01 / _mnt.m00;
+
+  _outImg.drawCircle(new cv.Point2(cx, cy), 8,
+    {
+      color: green,
+      thickness: 3
+    }
+  );
+
+  // Rect { height: 107, width: 148, y: 131, x: 101 }
+  let _bbox = _contour.boundingRect()
+
+  // console.log(`area : ${_contour.area}, isConvex : ${_contour.isConvex}`)
+
+  console.log(`arcLength : ${_contour.arcLength(true)}`);
+  console.log(`weight center : ${cx},${cy}`);
+  console.log('bounding rect :', _bbox);
+  // console.log(`solidity ${_contour.area}`);
+
+  //호의 길이 구하기 
+  _outImg.putText(
+    //String("arc :" + _contour.arcLength()),
+    `arcLength : ${ Math.round(_contour.arcLength() *100) / 100}`,
+    new cv.Point2(_bbox.x, _bbox.y - 8),
+    cv.FONT_HERSHEY_SIMPLEX,
+    0.5,
+    {
+      color: new cv.Vec(0, 255, 255),
+      thickness: 1
+    }
+  );
+
+  //오목형 이면 
+  if(!_contour.isConvex) {
+    //감쌀수있는 볼록 다각형구하기 
+    let _convexCountor =  _contour.convexHull();
+    //_convexCountor.area
+    _outImg.drawContours([_convexCountor.getPoints()], 0, green, 3);
+
+    //오목한 정도 구하기 1이면 거의 오목하지않다. 클수록 오목하다.
+    let _solidity =  Math.round((_convexCountor.area / _contour.area)*10)/10
+    _outImg.putText(
+      //String("arc :" + _contour.arcLength()),
+      `solidity : ${_solidity} `,
+      new cv.Point2(_bbox.x, _bbox.y - 22),
+      cv.FONT_HERSHEY_SIMPLEX,
+      0.5,
+      {
+        color: new cv.Vec(0, 255, 255),
+        thickness: 1
+      }
+    );
+
+  }
+  else {
+    _outImg.putText(
+      //String("arc :" + _contour.arcLength()),
+      'convex',
+      new cv.Point2(_bbox.x, _bbox.y - 22),
+      cv.FONT_HERSHEY_SIMPLEX,
+      0.5,
+      {
+        color: new cv.Vec(0, 255, 255),
+        thickness: 1
+      }
+    );
+
+  }
+
+  _outImg.drawRectangle(_contour.boundingRect(), {
+    color: blue,
     thickness: 3
-  }
-);
+  });
 
-console.log( `arcLength : ${_contour.arcLength(true)}`);
-console.log( `weight center : ${cx},${cy}`);
-console.log('bounding rect :',_contour.boundingRect() );
+})
 
-//호의 길이 구하기 
-_outImg.putText(
-  String("arc :" + _contour.arcLength()),
-  new cv.Point2(cx, cy),
-  cv.FONT_HERSHEY_SIMPLEX,
-  0.5,
-  {
-      color: new cv.Vec(0, 0, 0),
-      thickness: 2
-  }
-);
-
-_outImg.drawRectangle( _contour.boundingRect(), {
-  color : blue,
-  thickness : 3
-} );
-
-
-cv.imshowWait('countours',_outImg)
+cv.imwrite('../../temp/out.png', _outImg);
+// cv.imshowWait('countours',_outImg)
 
