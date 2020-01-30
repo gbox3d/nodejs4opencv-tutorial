96dc9e947c69d6aa62eadf08788ef3fa6ff4711f
gbox3d <gbox3d@gmail.com>
2020-01-30 15:25:19 +0900

    bug fix

--- basic/2.cam_snap.js
+++ basic/2.cam_snap.js
@@ -6,42 +6,43 @@ console.log(process.argv.length)
 // 라즈베리에서는 '/dev/video0' 이런식으로 인젝스 대신 디바이스이름을 직접 입력한다. 
 let devicePort = 0;
 
-if(process.argv.length >= 3)
-{
-    if(/^\d+$/.test(process.argv[2]) ) {
-        devicePort =  parseInt(process.argv[2]);
+if (process.argv.length >= 3) {
+    if (/^\d+$/.test(process.argv[2])) {
+        devicePort = parseInt(process.argv[2]);
     }
     else {
         devicePort = process.argv[2];
     }
-     
-    
+
+
 }
 
 console.log(`prepare capture device : ${devicePort} `);
 //
 const wCap = new cv.VideoCapture(devicePort);
 
-let _w,_h
-if(process.argv.length >= 5)
-{
+let _w, _h
+if (process.argv.length >= 5) {
     _w = parseInt(process.argv[3]);
-    _h = parseInt( process.argv[4] );
+    _h = parseInt(process.argv[4]);
+    wCap.set(cv.CAP_PROP_FRAME_WIDTH, _w)
+    wCap.set(cv.CAP_PROP_FRAME_HEIGHT, _h)
+}
+else {
+
 }
 
-wCap.set(cv.CAP_PROP_FRAME_WIDTH,_w)
-wCap.set(cv.CAP_PROP_FRAME_HEIGHT,_h)
 
 console.log('device  ok');
 
 
-while(1) {
+while (1) {
     let frame = wCap.read();
     console.log(frame);
-    
-    if(frame.cols > 0) {
+
+    if (frame.cols > 0) {
         cv.imwrite('../temp/out.png', frame);
-        
+
         break;
     }
 
