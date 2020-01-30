af5bf02e2fc4b363fdb4d5a636d0d554cbe8f740
gbox3d <gbox3d@gmail.com>
2020-01-30 15:17:31 +0900

    snap example param dev,w,h

--- basic/2.cam_snap.js
+++ basic/2.cam_snap.js
@@ -3,16 +3,34 @@ const cv = require('opencv4nodejs')
 
 console.log(process.argv.length)
 
+// 라즈베리에서는 '/dev/video0' 이런식으로 인젝스 대신 디바이스이름을 직접 입력한다. 
 let devicePort = 0;
 
 if(process.argv.length >= 3)
 {
-    devicePort = parseInt(process.argv[2]);
+    if(/^\d+$/.test(process.argv[2]) ) {
+        devicePort =  parseInt(process.argv[2]);
+    }
+    else {
+        devicePort = process.argv[2];
+    }
+     
     
 }
 
 console.log(`prepare capture device : ${devicePort} `);
+//
 const wCap = new cv.VideoCapture(devicePort);
+
+let _w,_h
+if(process.argv.length >= 5)
+{
+    _w = parseInt(process.argv[3]);
+    _h = parseInt( process.argv[4] );
+}
+
+wCap.set(cv.CAP_PROP_FRAME_WIDTH,_w)
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT,_h)
 
 console.log('device  ok');
 
