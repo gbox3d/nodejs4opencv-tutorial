b16117fb53d590f9bac2a86b6398bdde936b70d4
gbox3d <gbox3d@gmail.com>
2020-01-30 16:14:12 +0900

    ...

--- adv/camera/1_webserver.js
+++ adv/camera/1_webserver.js
@@ -4,24 +4,25 @@ const raspberryPiCamera = require('raspberry-pi-camera-native');
 
 //node 1_webserver.js 23081 3280 2464 1 
 
-const port = parseInt( process.argv[2])
-const cap_width = parseInt( process.argv[3])
-const cap_height = parseInt( process.argv[4])
-const cap_fps = parseInt( process.argv[5])
+const port = parseInt(process.argv[2])
+const cap_width = parseInt(process.argv[3])
+const cap_height = parseInt(process.argv[4])
+const cap_fps = parseInt(process.argv[5])
 
 
 raspberryPiCamera.start({
-  width: cap_width,
-  height: cap_height,
-  fps: cap_fps,
-  quality: 80,
-  encoding: 'JPEG'
+    width: cap_width,
+    height: cap_height,
+    fps: cap_fps,
+    quality: 80,
+    encoding: 'JPEG'
 });
 
 const server = http.createServer((req, res) => {
-  raspberryPiCamera.once('frame', (data) => {
-    res.end(data);
-  });
+    raspberryPiCamera.once('frame', (data) => {
+        console.log(`capture frame at ${(new Date())}`)
+        res.end(data);
+    });
 });
 
 
